b55d9ed3b6208f61b4f59e8b2f82dd70914a1830
Jason Barry <barryofguilder@gmail.com>
2019-01-16 12:48:55 -0500

    Use computed property for sort instead

--- app/components/color-form.js
+++ app/components/color-form.js
@@ -1,11 +1,14 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { task } from 'ember-concurrency';
+import { sort } from '@ember/object/computed';
 
 export default Component.extend({
   store: service(),
 
   colors: null,
+  colorSort: Object.freeze(['name']),
+  sortedColors: sort('colors', 'colorSort'),
   storeColors: null,
 
   didInsertElement() {
@@ -16,7 +19,7 @@ export default Component.extend({
 
   loadColors: task(function*() {
     let colors = yield this.store.loadAll('color');
-    this.set('colors', colors.sortBy('name'));
+    this.set('colors', colors);
 
     let storeColors = this.store.peekAll('color');
     this.set('storeColors', storeColors);
@@ -31,7 +34,8 @@ export default Component.extend({
 
     yield newColor.save();
 
-    this.colors.pushObject(newColor);
+    // Now that I'm not pushing the color to the array, the UI doesn't update.
+    //this.colors.pushObject(newColor);
 
     this.set('colorName', null);
   })
